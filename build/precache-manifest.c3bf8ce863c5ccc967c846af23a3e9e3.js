1aa67d5d78401a743f95798c1ef3f2c357ec9562
Devanadhen <projectdevanadhen@gmail.com>
2020-06-12 20:13:11 +0530

    Build

---
+++ build/precache-manifest.c3bf8ce863c5ccc967c846af23a3e9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319f7f9b682c00137ace96f7a6b3ab31",
+    "url": "/devaunivgit/production/index.html"
+  },
+  {
+    "revision": "f92114e1f78454f0ea45",
+    "url": "/devaunivgit/production/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19bd7bed6eb9ee0b3cb7",
+    "url": "/devaunivgit/production/static/js/2.7b022e0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/devaunivgit/production/static/js/2.7b022e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92114e1f78454f0ea45",
+    "url": "/devaunivgit/production/static/js/main.e00295a2.chunk.js"
+  },
+  {
+    "revision": "758d1a6d56961eaee4d8",
+    "url": "/devaunivgit/production/static/js/runtime-main.0e340dac.js"
+  }
+]);
